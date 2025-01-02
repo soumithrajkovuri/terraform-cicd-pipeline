658758c71cb1decc0ea99b11d7ac78e83dc90020
soumithrajkovuri <125905875+soumithrajkovuri@users.noreply.github.com>
2025-01-02 16:33:15 +0530

    pipeline

--- codepipeline.tf
+++ codepipeline.tf
@@ -1,6 +1,6 @@
 resource "aws_codebuild_project" "tf-plan" {
   name          = "tf-codebuild-plan"
-  service_role  = aws_iam_role.aws_iam_role.cicd_codebuild_role.arn
+  service_role  = aws_iam_role.cicd_codebuild_role.arn
 
   artifacts {
     type = "CODEPIPELINE"
@@ -30,7 +30,7 @@ resource "aws_codebuild_project" "tf-plan" {
 
 resource "aws_codebuild_project" "tf-apply" {
   name          = "tf-codebuild-apply"
-  service_role  = aws_iam_role.aws_iam_role.cicd_codebuild_role.arn
+  service_role  = aws_iam_role.cicd_codebuild_role.arn
 
   artifacts {
     type = "CODEPIPELINE"
