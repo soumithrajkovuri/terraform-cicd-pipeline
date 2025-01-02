3943a9e67301e69dfc592975d7cb43a356c61959
soumithrajkovuri <125905875+soumithrajkovuri@users.noreply.github.com>
2025-01-02 16:52:21 +0530

    pipeline

--- iam_role.tf
+++ iam_role.tf
@@ -1,8 +1,6 @@
 resource "aws_iam_role" "cicd_codepipeline_role" {
   name = "cicd_codepipeline_role"
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -13,10 +11,9 @@ resource "aws_iam_role" "cicd_codepipeline_role" {
         Principal = {
           Service = "codepipeline.amazonaws.com"
         }
-      },
+      }
     ]
   })
-
 }
 
 resource "aws_iam_role_policy" "cicd_codepipeline_policy" {
@@ -32,20 +29,17 @@ resource "aws_iam_role_policy" "cicd_codepipeline_policy" {
         Effect   = "Allow"
       },
       {
-        actions = ["cloudwatch:*", "s3:*", "codebuild:*"]
-        resources = ["*"]
-        effect = "Allow"
+        Action   = ["cloudwatch:*", "s3:*", "codebuild:*"]
+        Resource = ["*"]
+        Effect   = "Allow"
       }
     ]
   })
 }
 
-
 resource "aws_iam_role" "cicd_codebuild_role" {
-  name = "test_role"
+  name = "cicd_codebuild_role"
 
-  # Terraform's "jsonencode" function converts a
-  # Terraform expression result to valid JSON syntax.
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -56,26 +50,23 @@ resource "aws_iam_role" "cicd_codebuild_role" {
         Principal = {
           Service = "codebuild.amazonaws.com"
         }
-      },
+      }
     ]
   })
-
 }
 
 resource "aws_iam_role_policy" "cicd_codebuild_policy" {
   name = "cicd_codebuild_policy"
-  role = aws_iam_role.cicd_codepipeline_role.id
+  role = aws_iam_role.cicd_codebuild_role.id
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        
-        actions = ["logs:*", "s3:*", "codebuild:*", "secretsmanager:*","iam:*"]
-        resources = ["*"]
-        effect = "Allow"
+        Action   = ["logs:*", "s3:*", "codebuild:*", "secretsmanager:*", "iam:*"]
+        Resource = ["*"]
+        Effect   = "Allow"
       }
-      
     ]
   })
 }
